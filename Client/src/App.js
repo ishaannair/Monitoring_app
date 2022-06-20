67709aefe9d89d0b02a6424fd103c7e8950424e1
lanvoine <64321770+lanvoine@users.noreply.github.com>
2022-06-20 20:52:01 +0800

    Beginning Graph Editing

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,7 +3,8 @@ import './index.css';
 import Navbarfinal from './components/Navbar';
 import TestPage from './components/TestPage';
 import HomePage from './components/HomePage';
-
+import InsightsPage from './components/InsightsPage';
+import MyComponent from './components/InsightsTest';
 import React, { useState, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import {Route, BrowserRouter as Router,Routes ,Link} from "react-router-dom";
@@ -18,6 +19,8 @@ function App() {
         <Routes >
         <Route path="/" element={<HomePage/>} />
         <Route path="/test" element={<TestPage/>} />
+        <Route path="insights" element={<InsightsPage/>} />
+        <Route path="insightT" element={<MyComponent/>} />
       </Routes >
         
     
--- Client/src/components/HomePage.js
+++ Client/src/components/HomePage.js
@@ -83,10 +83,32 @@ const HomePage = (props) => {
         
             return (
         <div>
-           <div >testing</div>
-           <Button onClick={() =>{ navigate("test")}}>Test</Button>
-            
-        </div>
+            <Navbarfinal />
+            <div>
+                {/* <Layout>
+                    <Content style={{ padding: '0 50px' }}>
+                        <Button className='button-test' >Test</Button>
+                        <Button className='button-test' >Test</Button>
+                    </Content>
+                </Layout> */}
+                <Row>
+                    {/* <Col span={12}> */}
+                    <Link to="insights">
+                        <Button className='button-test button-test2' onClick={() =>{ navigate("example")}}>Data and Reports</Button>
+                    </Link>
+                    {/* </Col> */}
+                    {/* <Col span={12}> */}
+                    <Button className='button-test button-test2' >Platform Tracking</Button>
+                    {/* </Col> */}
+                    {/* <Col span={12}> */}
+                    <Button className='button-test button-test2' >Blog Posts</Button>
+                    {/* </Col> */}
+                    {/* <Col span={12}> */}
+                    <Button className='button-test button-test2' >User Profile</Button>
+                    {/* </Col> */}
+                </Row>
+            </div>
+        </div >
     );
 }
 
